a730ccf6b404621640127509dc1499a8211453d0
grammophone <emmanuel.liossis@gmail.com>
2017-02-16 05:51:24 +0200

    Add AsyncXXXCache variants.

---
+++ AsyncMRUCache.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grammophone.Caching
+{
+	/// <summary>
+	/// Thread-safe asynchronous cache of <typeparamref name="V"/> items indexed by 
+	/// key type <typeparamref name="K"/>. Items are kept up 
+	/// to a <see cref="MRUCache{K,V}.MaxCount"/>, above which the least recently 
+	/// used ones are evicted.
+	/// </summary>
+	/// <typeparam name="K">The type of item keys.</typeparam>
+	/// <typeparam name="V">the type of items cached.</typeparam>
+	/// <remarks>
+	/// The methods of this cache are thread-safe, suitable for parallel algorithms.
+	/// </remarks>
+	[Serializable]
+	public class AsyncMRUCache<K, V> : MRUCache<K, Task<V>>
+	{
+		/// <summary>
+		/// Create.
+		/// </summary>
+		/// <param name="asyncItemCreator">An asynchronous function to create an item corresponding to a key.</param>
+		/// <param name="maxCount">The maximum items count in the cache.</param>
+		public AsyncMRUCache(Func<K, Task<V>> asyncItemCreator, int maxCount = 1024)
+			: base(asyncItemCreator, maxCount)
+		{
+		}
+	}
+}
---
+++ AsyncSequentialMRUCache.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grammophone.Caching
+{
+	/// <summary>
+	/// Non-Thread-safe asynchronous cache of <typeparamref name="V"/> items indexed by 
+	/// key type <typeparamref name="K"/>. Items are kept up 
+	/// to a <see cref="SequentialMRUCache{K,V}.MaxCount"/>, above which the least recently 
+	/// used ones are evicted.
+	/// </summary>
+	/// <typeparam name="K">The type of item keys.</typeparam>
+	/// <typeparam name="V">The type of items cached.</typeparam>
+	/// <remarks>
+	/// The methods of this cache are not intended to be used by more than one
+	/// threads simultaneously.
+	/// </remarks>
+	public class AsyncSequentialMRUCache<K, V> : SequentialMRUCache<K, Task<V>>
+	{
+		/// <summary>
+		/// Create.
+		/// </summary>
+		/// <param name="asyncItemCreator">An asynchronous function to create an item corresponding to a key.</param>
+		/// <param name="maxCount">The maximum items count in the cache.</param>
+		public AsyncSequentialMRUCache(Func<K, Task<V>> asyncItemCreator, int maxCount = 1024) 
+			: base(asyncItemCreator, maxCount)
+		{
+		}
+	}
+}
